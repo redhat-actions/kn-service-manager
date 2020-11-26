7f7265b70ccf0fadfc0d01fe3678512a4b44b5d0
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-11-26 23:13:24 +0530

    feat: using pull secret
    - Ability to use private registry with the action
    - Create image pull secret and pass it to service
    - Handle extra service paramters

--- test/test.sh
+++ test/test.sh
@@ -3,45 +3,49 @@
 set -e 
 set -o pipefail 
 
-##############################################################
-## appendParams appends params to the kn service 
-##############################################################
+##########################
+##
+##########################
 function appendParams(){
    kn_command+=("$1")
 }
 
+INPUT_PRIVATE_REGISTRY="true"
+INPUT_K8S_API_SERVER_URL=$(oc whoami --show-server)
+INPUT_OPENSHIFT_TOKEN=$(oc whoami -t)
+INPUT_CONTAINER_IMAGE='quay.io/kameshsampath/fruits-app:master'
+INPUT_SERVICE_NAME='fun'
+INPUT_SERVICE_NAMESPACE='test'
+INPUT_REGISTRY_USER="$QUAYIO_USERNAME"
+INPUT_REGISTRY_PASSWORD="$QUAYIO_PASSWORD"
+INPUT_SERVICE_OPERATION='create'
+
 kn_command=("kn" "service")
+
+INPUT_SERVICE_PARAMS=$1
+
+#TODO how to login to kube servers??
+#TODO allow secure sigin
 
 if [[ -n "$INPUT_OPENSHIFT_TOKEN" ]];
 then
   echo "Using OpenShfit Token to login"
   oc login --insecure-skip-tls-verify "$INPUT_K8S_API_SERVER_URL" \
     --token="$INPUT_OPENSHIFT_TOKEN"
-elif [[ -n $INPUT_OPENSHIFT_USERNAME ]] && [[ -n $INPUT_OPENSHIFT_PASSWORD ]];
-then
+else 
   echo "Using OpenShfit Username and Password to login"
   oc login --insecure-skip-tls-verify "$INPUT_K8S_API_SERVER_URL" \
      --username="$INPUT_OPENSHIFT_USERNAME" \
      --password="$INPUT_OPENSHIFT_PASSWORD"
-else
-  #TODO Ability to log into kube cluster
-  echo "Login to the Kube Cluster"
 fi
 
-#################################################
-##
-#################################################
 registry_server="${INPUT_CONTAINER_IMAGE%/*/*}"
 secret_name="$registry_server.pull-secret"
 is_private_registry=false
 if [[ $INPUT_PRIVATE_REGISTRY == "yes" ]] || [[ $INPUT.PRIVATE_REGISTRY == "true" ]];
 then
- # delete the old secret if exist, that ensures 
- # new values are updated during each run
- kubectl delete secret "$secret_name" || true
- # create docker registry secret to allow pull
- # from private container registry 
- kubectl create secret docker-registry "$secret_name" \
+ oc delete secret "$secret_name" || true
+ oc create secret docker-registry "$secret_name" \
    --namespace="$INPUT_SERVICE_NAMESPACE" \
    --docker-username="$INPUT_REGISTRY_USER" \
    --docker-password="$INPUT_REGISTRY_PASSWORD" \
@@ -64,7 +68,6 @@ case $INPUT_SERVICE_OPERATION in
   ;;
 esac
 
-# Add an extra parameters to the service
 OLDIFS=$IFS
 if [[ -n $INPUT_SERVICE_PARAMS ]];
 then
@@ -72,11 +75,15 @@ then
   kn_command+=("${INPUT_SERVICE_PARAMS}")
   IFS=$OLDIFS
 fi
-
 echo "Running: ${kn_command[*]} "
 ${kn_command[*]}
 
-# After successful service creation extract the service url 
-# and set that as output to the action
+# Set the output 
 service_url=$(kn service describe --namespace="$INPUT_SERVICE_NAMESPACE" "$INPUT_SERVICE_NAME" -o url)
 echo "::set-output name=service_url::$service_url"
+
+http "$service_url/api/fruit"
+
+# # clean up
+kn service delete "$INPUT_SERVICE_NAME" \
+      --namespace="$INPUT_SERVICE_NAMESPACE"
