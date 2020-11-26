94a75c17243caceb116e716ae275f65d7f6d1b83
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-11-26 14:23:07 +0530

    skip tls verify for oc login

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,7 @@ set -o pipefail
 echo "$INPUT_SERVICE_PARAMS"
 
 #TODO how to login to kube servers??
+#TODO allow secure sigin
 
 if [[ -n "$INPUT_OPENSHIFT_TOKEN" ]];
 then
@@ -17,12 +18,14 @@ else
   echo "Using OpenShfit Username and Password to login"
   oc login --username="$INPUT_OPENSHIFT_USERNAME" \
      --password="$INPUT_OPENSHIFT_PASSWORD" \
+     --insecure-skip-tls-verify \
      "$INPUT_K8S_API_SERVER_URL"
 fi
 
 echo "Creating Knative Service $INPUT_SERVICE_NAME"
 kn service "$INPUT_SERVICE_OPERATION" "$INPUT_SERVICE_NAME" \
    --namespace="$INPUT_SERVICE_NAMESPACE" \
+   --insecure-skip-tls-verify \
    --image="$INPUT_CONTAINER_IMAGE"
 
 # Set the output 
