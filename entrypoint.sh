b052255773ec852f19a374f16d0f4fb6e261cb56
Kamesh Sampath <kamesh.sampath@hotmail.com>
2020-11-26 14:16:07 +0530

    prefix param env variables with INPUT

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,26 +4,27 @@ set -e
 set -o pipefail 
 
 #TODO combine with space seperation
-echo "$SERVICE_PARAMS"
+echo "$INPUT_SERVICE_PARAMS"
 
 #TODO how to login to kube servers??
 
-if [[ -n "$OPENSHIFT_TOKEN" ]];
+if [[ -n "$INPUT_OPENSHIFT_TOKEN" ]];
 then
   echo "Using OpenShfit Token to login"
-  oc login --token="$OPENSHIFT_TOKEN" "$K8S_API_SERVER_URL"
+  oc login --token="$INPUT_OPENSHIFT_TOKEN" \
+    "$INPUT_K8S_API_SERVER_URL"
 else 
   echo "Using OpenShfit Username and Password to login"
-  oc login --username="$OPENSHIFT_USERNAME" \
-     --password="$OPENSHIFT_PASSWORD"
-    "$K8S_API_SERVER_URL"
+  oc login --username="$INPUT_OPENSHIFT_USERNAME" \
+     --password="$INPUT_OPENSHIFT_PASSWORD" \
+     "$INPUT_K8S_API_SERVER_URL"
 fi
 
-echo "Creating Knative Service $SERVICE_NAME"
-kn service "$SERVICE_OPERATION" "$SERVICE_NAME" \
-   --namespace="$SERVICE_NAMESPACE" \
-   --image="$CONTAINER_IMAGE"
+echo "Creating Knative Service $INPUT_SERVICE_NAME"
+kn service "$INPUT_SERVICE_OPERATION" "$INPUT_SERVICE_NAME" \
+   --namespace="$INPUT_SERVICE_NAMESPACE" \
+   --image="$INPUT_CONTAINER_IMAGE"
 
 # Set the output 
-service_info=$(kn service describe --namespace="$SERVICE_NAMESPACE" "$SERVICE_NAME")
+service_info=$(kn service describe --namespace="$INPUT_SERVICE_NAMESPACE" "$INPUT_SERVICE_NAME")
 echo "::set-output name=service_info::$service_info"
